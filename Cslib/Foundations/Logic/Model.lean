c2f4157933d257c4b8e12c99fa313d58e634cc7d
twwar <tom.waring@unimelb.edu.au>
2026-05-21 11:04:59 +0200

    naming

--- Cslib/Foundations/Logic/Model.lean
+++ Cslib/Foundations/Logic/Model.lean
@@ -18,22 +18,22 @@ public section
 namespace Cslib.Logic
 
 /-- Objects of type `β` carry a forcing relation with the proposition type `α`. -/
-class ModelClass (α : outParam Type*) (β : Type*) where
-  /-- `Forces b a` has the intended semantics "`a` is valid in the model `b`". -/
-  Forces : β → α → Prop
+class Models (α : outParam Type*) (β : Type*) where
+  /-- `Satisfies b a` has the intended semantics "`a` is valid in the model `b`". -/
+  Satisfies : β → α → Prop
 
-scoped notation "⊨[" b "] " a => ModelClass.Forces b a
+scoped notation "⊨[" b "] " a => Models.Satisfies b a
 
 /-- Objects of type `β` carry a forcing relation worlds of type `γ` and the proposition type `α`. -/
-class ParamModelClass (α : outParam Type*) (β : Type*) where
+class ParamModels (α : outParam Type*) (β : Type*) where
   Param : β → Type*
   /-- Forcing relation associated to each parameter. -/
-  ForcesAt (b : β) : (Param b) → α → Prop
+  SatisfiesAt (b : β) : (Param b) → α → Prop
 
-scoped notation w " ⊨[" b "] " a => ParamModelClass.ForcesAt b w a
+scoped notation w " ⊨[" b "] " a => ParamModels.SatisfiesAt b w a
 
-instance ParamModelClass.toModelClass {α β : Type*} [ParamModelClass α β] : ModelClass α β where
-  Forces M A := ∀ w : ParamModelClass.Param M, w ⊨[M] A
+instance ParamModels.toModels {α β : Type*} [ParamModels α β] : Models α β where
+  Satisfies M A := ∀ w : ParamModels.Param M, w ⊨[M] A
 
 /-- Bundled interpretation function. -/
 class HasInterp (α : outParam Type*) (β : Type*) where
@@ -44,12 +44,12 @@ class HasInterp (α : outParam Type*) (β : Type*) where
 
 /-- An `InterpModel` consists of an interpretation function, and a set specifying which
   interpretations are considered valid. -/
-class InterpModelClass (α : outParam (Type*)) (β : Type*) extends HasInterp α β where
+class InterpModels (α : outParam (Type*)) (β : Type*) extends HasInterp α β where
   /-- The set of valid interpretations. -/
   filter (b : β) : Set (Ground b)
 
-instance InterpModelClass.instModelClass {α β : Type*} [InterpModelClass α β] : ModelClass α β where
-  Forces b a := HasInterp.interp b a ∈ filter b
+instance InterpModels.instModels {α β : Type*} [InterpModels α β] : Models α β where
+  Satisfies b a := HasInterp.interp b a ∈ filter b
 
 namespace HasInterp
 
@@ -67,29 +67,29 @@ class AlgebraicNot (α β : Type*) [HasInterp α β] [HasNot α] [∀ b : β, Co
 
 end HasInterp
 
-open ModelClass ParamModelClass InferenceSystem
+open Models ParamModels InferenceSystem
 
-variable {α β T} [ModelClass α β] [InferenceSystem T α]
+variable {α β T} [Models α β] [InferenceSystem T α]
 
-def SoundFor (α β T) [ModelClass α β] [InferenceSystem T α] (S : Set β) : Prop :=
+def SoundFor (α β T) [Models α β] [InferenceSystem T α] (S : Set β) : Prop :=
   ∀ (A : α), DerivableIn T A → ∀ M ∈ S, ⊨[M] A
 
 lemma SoundFor.subset {S S' : Set β} (hS : S ⊆ S') : SoundFor α β T S' → SoundFor α β T S :=
   fun h A hA M hM => h A hA M (hS hM)
 
-def CompleteFor (α β T : Type*) [ModelClass α β] [InferenceSystem T α] (S : Set β) : Prop :=
+def CompleteFor (α β T : Type*) [Models α β] [InferenceSystem T α] (S : Set β) : Prop :=
   ∀ A : α, (∀ M ∈ S, ⊨[M] A) → DerivableIn T A
 
 lemma CompleteFor.supset {S S' : Set β} (hS : S ⊆ S') :
     CompleteFor α β T S → CompleteFor α β T S' := fun h A hA => h A (fun M hM => hA M (hS hM))
 
-def IsCompleteModel (α β T) [ModelClass α β] [InferenceSystem T α] (M : β) : Prop :=
+def IsCompleteModel (α β T) [Models α β] [InferenceSystem T α] (M : β) : Prop :=
   ∀ (A : α), (⊨[M] A) → DerivableIn T A
 
-def ParamModelClass.theory {α β : Type*} [ParamModelClass α β] {M : β} (w : Param M) : Set α :=
+def ParamModels.theory {α β : Type*} [ParamModels α β] {M : β} (w : Param M) : Set α :=
   {A : α | w ⊨[M] A}
 
-def ModelClass.logic {α β : Type*} [ModelClass α β] (S : Set β) : Set α := {A | ∀ b ∈ S, ⊨[b] A}
+def Models.logic {α β : Type*} [Models α β] (S : Set β) : Set α := {A | ∀ b ∈ S, ⊨[b] A}
 
 namespace Modal
 
@@ -100,18 +100,18 @@ structure BundledModel (Atom : Type*) where
 def Model.toBundledModel {World Atom : Type*} (M : Model World Atom) : BundledModel Atom :=
   {World := World, model := M}
 
-instance {Atom : Type*} : ParamModelClass (Proposition Atom) (BundledModel Atom) where
+instance {Atom : Type*} : ParamModels (Proposition Atom) (BundledModel Atom) where
   Param M := M.World
-  ForcesAt M w A := Satisfies M.model w A
+  SatisfiesAt M w A := Satisfies M.model w A
 
 example {World Atom : Type*} (S : Set (Model World Atom)) :
-    logic S = ModelClass.logic (Model.toBundledModel '' S) := by
-  simp [ModelClass.logic]
+    logic S = Models.logic (Model.toBundledModel '' S) := by
+  simp [Models.logic]
   rfl
 
 example {World Atom : Type*} (m : Model World Atom) (w : World) :
-    theory m w = ParamModelClass.theory (M := m.toBundledModel) w := by
-  simp [theory, ParamModelClass.theory]
+    theory m w = ParamModels.theory (M := m.toBundledModel) w := by
+  simp [theory, ParamModels.theory]
   rfl
 
 end Modal
@@ -138,7 +138,7 @@ def HeytingModel.interp (M : HeytingModel Atom) : Proposition Atom → M.H
   | Proposition.or A B => M.interp A ⊔ M.interp B
   | Proposition.impl A B => M.interp A ⇨ M.interp B
 
-instance : InterpModelClass (Proposition Atom) (HeytingModel Atom) where
+instance : InterpModels (Proposition Atom) (HeytingModel Atom) where
   Ground M := M.H
   interp := HeytingModel.interp
   filter _ := {⊤}
